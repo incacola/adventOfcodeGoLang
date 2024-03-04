0797ad9a9df859d818d7b40b2138385da946adf8
Felipe Gil Gutierrez <felipe.gil@arvato.com>
2024-03-04 12:23:37 +0100

    :zap: day 1 initial commit :zap:

---
+++ day1/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// Function que nos de el resultado de una calibration
+// input (1abc) return 12
+func calibration(calibrationLine string) int {
+	// pasa algo para convertir el String a Int
+	return 12
+}
+
+func main() {
+	// Hacer una variable con todo el contenido del archivo
+	// calibrations.txt
+
+	// Array con las cuatro lineas del ejemplo
+	// Mandar el array cada linea con un Loop a la function de calibration
+	// Poner el resultado en un array nuevo con los valores
+
+	// Summar los valores
+
+	// Prinln del resultado en la consola
+	fmt.Println("Hello, world.")
+}
